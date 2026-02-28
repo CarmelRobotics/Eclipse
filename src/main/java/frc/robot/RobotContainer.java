ed94383d69bf463993b77e0d66f9ec67dbf56143
Thaddeus <thumphreyschmidt@cusd.me>
2026-02-28 08:49:04 -0800

    drive

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,8 +16,8 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
-import frc.robot.subsystems.drive.LocalisedSwerveConstants;
-import frc.robot.subsystems.drive.LocalisedSwerveDrivetrain;
+import frc.robot.subsystems.drive.CommandSwerveDrivetrain;
+import frc.robot.subsystems.drive.TunerConstants;
 import frc.robot.subsystems.feeder.Feeder;
 import frc.robot.subsystems.feeder.FeederConstants.FeederState;
 import frc.robot.subsystems.lintake.Lintake;
@@ -28,19 +28,17 @@ import frc.robot.subsystems.shooter.ShooterConstants.IndexerState;
 import frc.robot.subsystems.shooter.ShooterConstants.PivotState;
 import frc.robot.subsystems.shooter.ShooterConstants.ShooterState;
 
-public class RobotContainer {/* 
-  private final double MaxSpeed = 1.0 * LocalisedSwerveConstants.kSpeedAt12Volts.in(MetersPerSecond);
-  private final double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond);
-  private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1)
-            .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
-  private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
-  private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
-  */
+public class RobotContainer {
+  private static final double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
+  private static final double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
-  //private final LocalisedSwerveDrivetrain m_drivetrain = new LocalisedSwerveDrivetrain();
-  //private final Shooter m_shooter = new Shooter(m_drivetrain::getPose);
-  //private final LocalisedSwerveDrivetrain m_drive = new LocalisedSwerveDrivetrain();
+  /* Setting up bindings for necessary control of the swerve drive platform */
+  private final SwerveRequest.FieldCentric m_drive = new SwerveRequest.FieldCentric()
+            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+            .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+
+  private final CommandSwerveDrivetrain m_drivetrain = TunerConstants.createDrivetrain();
+
   private final Lintake m_lintake = new Lintake();
   private final Shooter m_shooter = new Shooter(() -> new Pose2d());
   private final Feeder m_feeder = new Feeder();
@@ -51,26 +49,24 @@ public class RobotContainer {/*
   }
 
   private void configureBindings() {
-    /*
-    m_drive.setDefaultCommand(
+    m_drivetrain.setDefaultCommand(
             // Drivetrain will execute this command periodically
-            m_drive.applyRequest(() ->
-                drive.withVelocityX(-m_controller.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
+            m_drivetrain.applyRequest(() ->
+                m_drive.withVelocityX(-m_controller.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
                     .withVelocityY(-m_controller.getLeftX() * MaxSpeed) // Drive left with negative X (left)
                     .withRotationalRate(-m_controller.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
             )
         );
 
-    final var idle = new SwerveRequest.Idle();
-        RobotModeTriggers.disabled().whileTrue(
-            m_drive.applyRequest(() -> idle).ignoringDisable(true)
+
+    final SwerveRequest idle = new SwerveRequest.Idle();
+    RobotModeTriggers.disabled().whileTrue(
+            m_drivetrain.applyRequest(() -> idle).ignoringDisable(true)
         );
         
     
-    m_controller.leftBumper().onTrue(m_drive.runOnce(m_drive::seedFieldCentric));
-    */
-    
-    
+    m_controller.povDown().onTrue(m_drivetrain.runOnce(m_drivetrain::seedFieldCentric));
+
     m_controller.leftBumper().onTrue(Commands.run(() -> m_lintake.setState(PinionState.GROUND), m_lintake));
     m_controller.rightBumper().onTrue(Commands.run(() -> m_lintake.setState(PinionState.STOW), m_lintake));
     m_controller.rightTrigger().onTrue(Commands.run(() -> m_shooter.setState(PivotState.SCORE), m_shooter));
@@ -79,8 +75,10 @@ public class RobotContainer {/*
     m_controller.b().onTrue(Commands.run(() -> m_shooter.setState(ShooterState.ZERO), m_shooter));
     m_controller.x().onTrue(Commands.run(() -> m_shooter.setState(IndexerState.SCORE), m_shooter));
     m_controller.y().onTrue(Commands.run(() -> m_shooter.setState(IndexerState.ZERO), m_shooter));
+    /*
     m_controller.povUp().onTrue(Commands.run(() -> m_feeder.setState(FeederState.SCORE), m_feeder));
     m_controller.povDown().onTrue(Commands.run(() -> m_feeder.setState(FeederState.ZERO), m_feeder));
+    */
     m_controller.povLeft().onTrue(Commands.run(() -> m_lintake.setState(RollerState.INTAKE), m_lintake));
     m_controller.povRight().onTrue(Commands.run(() -> m_lintake.setState(RollerState.ZERO), m_lintake));
   }
